55a84050089f24902d59e0a76a0d506df66e5212
abhiabhish1234 <42028071+abhiabhish1234@users.noreply.github.com>
2019-02-22 14:30:04 +0530

    Create ajjj.c

---
+++ ajjj.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argu, char *arguv[])
+{
+ size_t maxlen = 0, leng;
+ int k;
+ int longest;
+ for (k = 1; k < arg; k++) 
+  {
+  len = strlen(arguv[k]);
+  if (leng>maxlen) 
+  longest = arguv[k];
+ }
+printf("longest string is %s. \n", longest);
+return 0;
+}
